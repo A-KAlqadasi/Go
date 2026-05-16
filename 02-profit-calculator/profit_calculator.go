a010f50aec6083ef700fd0d28afed99e80b502c9
A-KAlqadsi <abdulkareemalqa@gmail.com>
2026-05-16 11:51:46 +0300

    feat(02-profit-calculator) adding the profits into file

--- 02-profit-calculator/profit_calculator.go
+++ 02-profit-calculator/profit_calculator.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 
@@ -10,17 +13,38 @@ func main() {
 	var taxRate float64
 
 	revenue = readInput("Enter revenue: ")
+	if !isValidInput(revenue) {
+		fmt.Println("Invalid input for revenue. Please enter a positive number.")
+		return
+	}
 	expenses = readInput("Enter expenses: ")
-	taxRate = readInput("Enter tax rate: ")
+	if !isValidInput(expenses) {
+		fmt.Println("Invalid input for expenses. Please enter a positive number.")
+		return
+	}
 
-	// profitBeforeTax := revenue - expenses
-	// taxAmount := profitBeforeTax * (taxRate / 100)
-	// profitAfterTax := profitBeforeTax - taxAmount
+	taxRate = readInput("Enter tax rate: ")
+	if !isValidInput(taxRate) {
+		fmt.Println("Invalid input for tax rate. Please enter a positive number.")
+		return
+	}
+
 	profitBeforeTax, taxAmount, profitAfterTax := calculateProfits(revenue, expenses, taxRate)
-	
 	printOutput("Tax: ", taxAmount)
 	printOutput("Profit Before Tax: ", profitBeforeTax)
 	printOutput("Profit After Tax: ", profitAfterTax)
+	
+	writeProfitsToFile(profitBeforeTax, taxAmount, profitAfterTax)
+
+}
+
+func writeProfitsToFile(profitBeforeTax float64, taxAmount float64, profitAfterTax float64) {
+	content := fmt.Sprintf("Profit Before Tax: %v\nTax Amount: %v\nProfit After Tax: %v\n", profitBeforeTax, taxAmount, profitAfterTax)
+
+	err :=os.WriteFile("profits.txt", []byte(content), 0644)
+	if err != nil {
+		panic("Failed to write to file")
+	}
 
 }
 
@@ -37,6 +61,10 @@ func readInput(prompt string) float64 {
 	fmt.Print(prompt)
 	fmt.Scanln(&value)
 	return value
+}
+
+func isValidInput(value float64) bool {
+	return value > 0
 }
 
 func printOutput(message string, value float64) {
